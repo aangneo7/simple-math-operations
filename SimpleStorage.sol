5009193a6e791b027d7f86e32308b9b2feeaebe8
aangneo7 <113002498+aangneo7@users.noreply.github.com>
2024-05-17 00:58:31 +0700

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+echo '// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private storedValue;
+
+    function set(uint256 value) public {
+        storedValue = value;
+    }
+
+    function get() public view returns (uint256) {
+        return storedValue;
+    }
+}' > SimpleStorage.sol
